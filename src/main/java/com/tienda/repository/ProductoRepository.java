235dfa5b574fe50f53aebc642f8e2b33b491a453
caleb <caleb@Scorpion.bbrouter>
2026-06-30 19:40:21 -0600

    Commit 08: cambios de clase

--- src/main/java/com/tienda/repository/ProductoRepository.java
+++ src/main/java/com/tienda/repository/ProductoRepository.java
@@ -3,11 +3,22 @@ package com.tienda.repository;
 import com.tienda.domain.Producto;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 
     public List<Producto> findByActivoTrue();
-    
+
+    //Ejemplo de metodo utilizando consultas derivadas
+    public List<Producto> findByPrecioBetweenOrderByPrecioAsc(double precioInf, double precioSup);
+
+     //Ejemplo de metodo utilizando consultas JPQL
+    @Query(value = "SELECT p FROM Producto p WHERE p.precio BETWEEN :precioInf AND :precioSup ORDER BY p.precio ASC")
+    public List<Producto> consultaJPQL(@Param("precioInf")double precioInf, @Param("precioSup")double precioSup);
+
+    //Ejemplo consulta nativa utilizando consultas sql nativas
+    @Query(nativeQuery = true,
+            value = "SELECT * FROM producto p WHERE p.precio BETWEEN :precioInf AND :precioSup ORDER BY p.precio ASC")
+    public List<Producto> consultaSQL(@Param ("precioInf")double precioInf,@Param("precioSup") double precioSup);
 }
--- src/main/java/com/tienda/service/ProductoService.java
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -65,4 +65,23 @@ public class ProductoService {
             throw new IllegalStateException("No se puede eliminar el producto. Tiene datos asociados.", e);
         }
     }
+    
+    @Transactional(readOnly = true)
+    public List<Producto> consultaDerivada(double precioInf, double precioSup) {
+        return productoRepository.findByPrecioBetweenOrderByPrecioAsc(precioInf, precioSup);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Producto> consultaJPQL(double precioInf, double precioSup) {
+        return productoRepository.consultaJPQL(precioInf, precioSup);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Producto> consultaSQL(double precioInf, double precioSup) {
+        return productoRepository.consultaSQL(precioInf, precioSup);
+    }
+
 }
+
+
+
