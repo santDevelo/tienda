62543beac8b7963b2073580e2356c0b60134ec20
caleb <caleb@Scorpion.bbrouter>
2026-07-22 10:54:52 -0600

    Commit 11:Cambios semana 11

--- src/main/java/com/tienda/repository/UsuarioRepository.java
+++ src/main/java/com/tienda/repository/UsuarioRepository.java
@@ -2,6 +2,7 @@ package com.tienda.repository;
 
 import com.tienda.domain.Usuario;
 import java.util.Optional;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,14 @@ import org.springframework.stereotype.Repository;
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     
     public Optional<Usuario> findByUsernameAndActivoTrue(String username);
-    
+
+    public List<Usuario> findByActivoTrue();
+
+    public Optional<Usuario> findByUsername(String username);
+
+    public Optional<Usuario> findByUsernameAndPassword(String username, String Password);
+
+    public Optional<Usuario> findByUsernameOrCorreo(String username, String correo);
+
+    public boolean existsByUsernameOrCorreo(String username, String correo);
 }
